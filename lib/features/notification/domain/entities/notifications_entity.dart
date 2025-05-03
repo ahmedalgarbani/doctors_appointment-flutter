c69a544ad6c02403344d2c140707942f56edccd6
yasser <yasser.aloniass@gmail.com>
2025-05-03 16:26:13 +0300

    aa

--- lib/features/notification/domain/entities/notifications_entity.dart
+++ lib/features/notification/domain/entities/notifications_entity.dart
@@ -2,10 +2,16 @@ class NotificationsEntity {
   final String message;
   final String status;
   final String notificationType;
+  final String createdAt;
+  final String senderName;
+  final String senderEmail;
 
   NotificationsEntity({
     required this.message,
     required this.status,
     required this.notificationType,
+    required this.createdAt,
+    required this.senderName,
+    required this.senderEmail,
   });
 }
